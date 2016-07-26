72f4535a0d09b3458def883427274571f8e426df
Harry Brodsky <hbrodsk1@binghamton.edu>
2016-07-26 01:10:50 -0400

    Update logic to move piece to open space

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,45 +1,42 @@
-
-
 class Board
-  attr_reader :board
+  attr_accessor :board
 
   def initialize
     #Create 8x8 game board as a 2d array
     @board = Array.new(8){Array.new(8)}
     populate
-    display
   end
 
   def populate
     #Populates board rows 1 & 6 with Pawns
-    @board[1].map! { |pawn_space| pawn_space = Pawn.new("Black", "265F").unicode }
-    @board[6].map! { |pawn_space| pawn_space = Pawn.new("White", "2659").unicode }
+    @board[1].map! { |pawn_space| pawn_space = Pawn.new("Black", "265F") }
+    @board[6].map! { |pawn_space| pawn_space = Pawn.new("White", "2659") }
 
     #Populates board spaces [0][0], [0][7], [7][0], [7][7] with Rookes
-    @board[0][0] = Rooke.new("Black", "265C").unicode
-    @board[0][7] = Rooke.new("Black", "265C").unicode
-    @board[7][0] = Rooke.new("White", "2656").unicode
-    @board[7][7] = Rooke.new("White", "2656").unicode
+    @board[0][0] = Rooke.new("Black", "265C")
+    @board[0][7] = Rooke.new("Black", "265C")
+    @board[7][0] = Rooke.new("White", "2656")
+    @board[7][7] = Rooke.new("White", "2656")
 
     #Populates board spaces [0][1], [0][6], [7][1], [7][6] with Knights
-    @board[0][1] = Knight.new("Black", "265E").unicode
-    @board[0][6] = Knight.new("Black", "265E").unicode
-    @board[7][1] = Knight.new("White", "2658").unicode
-    @board[7][6] = Knight.new("White", "2658").unicode
+    @board[0][1] = Knight.new("Black", "265E")
+    @board[0][6] = Knight.new("Black", "265E")
+    @board[7][1] = Knight.new("White", "2658")
+    @board[7][6] = Knight.new("White", "2658")
 
     #Populates board spaces [0][2], [0][5], [7][2], [7][5] with Bishops
-    @board[0][2] = Bishop.new("Black", "265D").unicode
-    @board[0][5] = Bishop.new("Black", "265D").unicode
-    @board[7][2] = Bishop.new("White", "2657").unicode
-    @board[7][5] = Bishop.new("White", "2657").unicode
+    @board[0][2] = Bishop.new("Black", "265D")
+    @board[0][5] = Bishop.new("Black", "265D")
+    @board[7][2] = Bishop.new("White", "2657")
+    @board[7][5] = Bishop.new("White", "2657")
 
     #Populates board spaces [0][3], [7][3] with Queens
-    @board[0][3] = Queen.new("Black", "265B").unicode
-    @board[7][3] = Queen.new("White", "2655").unicode
+    @board[0][3] = Queen.new("Black", "265B")
+    @board[7][3] = Queen.new("White", "2655")
 
     #Populates board spaces [0][4], [7][4] with Kings
-    @board[0][4] = King.new("Black", "265A").unicode
-    @board[7][4] = King.new("White", "2654").unicode
+    @board[0][4] = King.new("Black", "265A")
+    @board[7][4] = King.new("White", "2654")
   end
 
   def display
@@ -55,7 +52,12 @@ class Board
       print "   #{row}"
       print "   "
       (0..7).each do |col|
-        print @board[row][col]
+        if @board[row][col].class.superclass == Piece
+          print @board[row][col].unicode
+        else
+          print @board[row][col]
+        end
+
         print "  |  " if !@board[row][col].nil? unless col == 7
         print "   |  " if @board[row][col].nil? unless col == 7
       end
@@ -67,15 +69,18 @@ class Board
 end
 
 class Player
-  attr_reader :name
+  attr_reader :name, :color
 
-  def initialize(name)
-    @name = name
+  def initialize(color)
+    @name = ""
+    @color = color
   end
 
-  def turn_message
-    puts "Your turn, #{@name}. Please select the piece you would like to move\n
-    by typing in its coordinates, separated by a comma and a space. ie. 6, 2"
+  def set_name
+    puts "Please enter your name..."
+    @name = gets.chomp
+
+    #return @name
   end
 end
 
@@ -90,7 +95,7 @@ class Piece
 end
 
 class Pawn < Piece
-  def move(current_space, end_space)
+  def self.move(current_space, end_space)
 
   end
 
@@ -116,4 +121,126 @@ class King < Piece
 
 end
 
-Board.new
+class Game
+
+  def initialize
+    @player_1 = ""
+    @player_2 = ""
+    @current_player = ""
+    @current_color = ""
+    @piece_to_move = ""
+    @space_to_move_to = ""
+    @board = Board.new
+  end
+
+  def welcome_message
+    puts "\n\nWelcome to chess!\n\nPlayer 1, what would you like to be called?\n"
+
+    @player_1 = Player.new("Black") ; @player_1.set_name
+    @current_player = @player_1.name
+    @current_color = @player_1.color
+
+    puts "\nThank you #{@player_1.name}"
+
+    puts "\nPlayer 2, what would you like to be called?\n"
+
+    @player_2 = Player.new("White") ; @player_2.set_name
+    puts "\nThank you #{@player_2.name}\n\n"
+  end
+
+  def turn_message
+    #@board.board[2][2] = Pawn.new("Black", "265F")
+    #@board.board[1][2] = nil
+    #@board.display
+    @board.display
+    puts "Your turn, #{@current_player}. Please select the piece you would like to move\nby typing in its coordinates, separated by a comma and a space. ie. 6, 2\n\n"
+
+    find_piece_type
+  end
+
+  def find_piece_type
+    if format_origin_space_input && format_destination_space_input
+      @piece_class = @board.board[@piece_to_move[0]][@piece_to_move[1]].class
+      
+      @board.board[@space_to_move_to[0]][@space_to_move_to[1]] = @board.board[@piece_to_move[0]][@piece_to_move[1]]
+      @board.board[@piece_to_move[0]][@piece_to_move[1]] = nil
+      @board.display
+    else
+      turn_message 
+    end
+  end
+
+  def format_origin_space_input
+    acceptable_input = ["0", "1", "2", "3", "4", "5", "6", "7"]
+    @piece_to_move = gets.chomp.split(", ")
+
+    if @piece_to_move.length == 2 && @piece_to_move.all? { |num| acceptable_input.include?(num) }
+      @piece_to_move = @piece_to_move.map! { |string| string.to_i }
+      return validate_piece_to_move_input(@piece_to_move[0], @piece_to_move[1])
+    else
+      error_message("bad range")
+      return false
+    end
+  end
+
+  def format_destination_space_input
+    acceptable_input = ["0", "1", "2", "3", "4", "5", "6", "7"]
+
+    puts "Please select the space you would like to move to\nby typing in its coordinates, separated by a comma and a space. ie. 6, 2\n\n"
+    @space_to_move_to = gets.chomp.split(", ")
+
+    if @space_to_move_to.length == 2 && @space_to_move_to.all? { |num| acceptable_input.include?(num) }
+      @space_to_move_to = @space_to_move_to.map! { |string| string.to_i }
+      return validate_space_to_move_to(@space_to_move_to[0], @space_to_move_to[1])
+    else
+      error_message("bad range")
+      return false
+    end
+    #@board.board[@piece_to_move[0]][@piece_to_move[1]] = nil
+    #@board.display
+  end
+
+  def validate_piece_to_move_input(input_1, input_2)
+    unless input_1.between?(0, 7) && input_2.between?(0, 7)
+      return false
+    end
+
+    if !@board.board[input_1][input_2].nil?
+      return true
+    else
+      error_message("no piece")
+      return false
+    end
+  end
+
+  def validate_space_to_move_to(input_1, input_2)
+    if @board.board[input_1][input_2].nil?
+      return true
+    #else
+     # error_message("no piece")
+      #return false
+    end
+  end
+
+  def error_message(error_type)
+    case error_type
+    when "bad range"
+      #@board.display
+      puts "\n\nPlease make sure consists of two numbers betweem 0 and 7"
+    when "no piece"
+      #@board.display
+      puts "\n\nYou must enter a space that currently has one of your pieces"
+    end
+  end
+
+  def switch_players
+    if @current_player == @player_1.name
+      @current_player = @player_2.name
+    elsif @current_player == @player_2.name
+      @current_player = @player_1.name
+    end
+  end
+end
+
+
+x = Game.new ; x.welcome_message ; x.turn_message
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -16,56 +16,69 @@ describe Board do
 		context "at beginning of game" do
 			it "populates board rows 1 & 6 with Pawns" do
 				subject[1].each do |type|
-				expect(type).to be_a(Pawn)
+				expect(type.class).to eq(Pawn)
 				end
 				subject[6].each do |type|
-				expect(type).to be_a(Pawn)
+				expect(type.class).to eq(Pawn)
 				end
 			end
 
 			it "populates board spaces [0][0], [0][7], [7][0], [7][7] with Rookes" do
-				expect(subject[0][0]).to be_a(Rooke)
-				expect(subject[0][7]).to be_a(Rooke)
-				expect(subject[7][0]).to be_a(Rooke)
-				expect(subject[7][7]).to be_a(Rooke)
+				expect(subject[0][0].class).to eq(Rooke)
+				expect(subject[0][7].class).to eq(Rooke)
+				expect(subject[7][0].class).to eq(Rooke)
+				expect(subject[7][7].class).to eq(Rooke)
 			end
 
 			it "populates board spaces [0][1], [0][6], [7][1], [7][6] with Knights" do
-				expect(subject[0][1]).to be_a(Knight)
-				expect(subject[0][6]).to be_a(Knight)
-				expect(subject[7][1]).to be_a(Knight)
-				expect(subject[7][6]).to be_a(Knight)
+				expect(subject[0][1].class).to eq(Knight)
+				expect(subject[0][6].class).to eq(Knight)
+				expect(subject[7][1].class).to eq(Knight)
+				expect(subject[7][6].class).to eq(Knight)
 			end
 
 			it "populates board spaces [0][2], [0][5], [7][2], [7][5] with Bishops" do
-				expect(subject[0][2]).to be_a(Bishop)
-				expect(subject[0][5]).to be_a(Bishop)
-				expect(subject[7][2]).to be_a(Bishop)
-				expect(subject[7][5]).to be_a(Bishop)
+				expect(subject[0][2].class).to eq(Bishop)
+				expect(subject[0][5].class).to eq(Bishop)
+				expect(subject[7][2].class).to eq(Bishop)
+				expect(subject[7][5].class).to eq(Bishop)
 			end
 
 			it "populates board spaces [0][3], [7][3] with Queens" do
-				expect(subject[0][3]).to be_a(Queen)
-				expect(subject[7][3]).to be_a(Queen)
+				expect(subject[0][3].class).to eq(Queen)
+				expect(subject[7][3].class).to eq(Queen)
 			end
 
 			it "populates board spaces [0][4], [7][4] with Kings" do
-				expect(subject[0][4]).to be_a(King)
-				expect(subject[7][4]).to be_a(King)
+				expect(subject[0][4].class).to eq(King)
+				expect(subject[7][4].class).to eq(King)
 			end
 		end
 	end
 end
 
 describe Player do
-	let(:player) { Player.new("Dingo") }
+	let(:player) { Player.new("Black") }
 	describe "#new" do
 		context "beginning of game" do
-			it "sets player's name" do
-				expect(player.name).to eq("Dingo")
+			it "sets player's color" do
+				expect(player.color).to eq("Black")
 			end
 		end
 	end
+
+	describe "#set_name" do
+		before do
+      		io_obj = double
+      		expect(player).to receive(:gets).and_return(io_obj)
+      		expect(io_obj).to receive(:chomp).and_return("Harry")
+    	end
+			it "returns @name" do
+				player.set_name
+				expect(player.instance_variable_get(:@name)).to eq("Harry")
+			end
+		end
+	
 end
 
 describe Piece do
@@ -81,9 +94,18 @@ describe Piece do
 end
 
 describe Pawn do
+	let(:pawn) { Pawn.new }
 	context "Check that it inherits from Piece class" do
 		it { expect(Pawn.superclass).to be Piece }
 	end
+
+	describe "#move" do
+		context "regular move" do
+			it "allows pawn to move one space forward if space is unoccupied" do
+			end
+		end
+	end
+
 end
 
 describe Rooke do
@@ -113,6 +135,50 @@ end
 describe King do
 	context "Check that it inherits from Piece class" do
 		it { expect(King.superclass).to be Piece }
+	end
+end
+
+describe Game do
+	let(:game) { Game.new }
+	describe "#validate_piece_to_move_input" do
+		context "proper range of numbers is given" do
+			it "returns true" do
+				expect(game.validate_piece_to_move_input(1, 2)).to eq(true)
+			end
+		end
+
+		context "improper range of numbers is given" do
+			it "returns false" do
+				expect(game.validate_piece_to_move_input(8, 9)).to eq(false)
+			end
+		end
+
+		context "space contains a piece to be moved" do
+			it "returns true" do
+				expect(game.validate_piece_to_move_input(1, 6)).to eq(true)
+			end
+		end
+
+		context "space does not contain a piece to be moved" do
+			it "returns false" do
+				expect(game.validate_piece_to_move_input(3, 3)).to eq(false)
+			end
+		end
+	end
+
+	describe "#switch_players" do
+		context "@current_player is player_1.name" do
+			it "switches @current_player to @player_2.name" do
+				@player_1 = game.instance_variable_set(:@player_1, Player.new("Black"))
+				allow(@player_1).to receive(:name).and_return("Harry")
+				@player_2 = game.instance_variable_set(:@player_2, Player.new("White"))
+				allow(@player_2).to receive(:name).and_return("Nick")
+	
+				game.instance_variable_set(:@current_player, @player_1.name)
+				game.switch_players
+				expect(game.instance_variable_get(:@current_player)).to eq(@player_2.name)
+			end
+		end
 	end
 end
 
