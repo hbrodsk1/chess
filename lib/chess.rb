d66a7e2ce093162c19ad4dfe0a6b0a3dfb6c082e
Harry Brodsky <hbrodsk1@binghamton.edu>
2016-07-26 17:12:41 -0400

    Pawn can move and capturepieces

--- lib/chess.rb
+++ lib/chess.rb
@@ -69,11 +69,12 @@ class Board
 end
 
 class Player
-  attr_reader :name, :color
+  attr_reader :name, :color, :captured_pieces
 
   def initialize(color)
     @name = ""
     @color = color
+    @captured_pieces = []
   end
 
   def set_name
@@ -95,10 +96,37 @@ class Piece
 end
 
 class Pawn < Piece
-  def self.move(current_space, end_space)
-
+  def move?(current_space, end_space, color)
+    case color
+    when "Black"
+      if end_space[0] == current_space[0] + 1 && end_space[1] == current_space [1]
+        return true
+      end
+    when "White"
+      if end_space[0] == current_space[0] - 1 && end_space[1] == current_space [1]
+        return true
+      end
+    end  
+  false 
   end
 
+  def attackable_piece?(current_space, end_space, color, board)
+    case color
+    when "Black"
+      if end_space[0] == current_space[0] + 1 && end_space[1] == current_space [1] + 1 && board.board[end_space[0]][end_space[1]].color != "Black"
+        return true
+      elsif end_space[0] == current_space[0] + 1 && end_space[1] == current_space [1] - 1 && board.board[end_space[0]][end_space[1]].color != "Black"
+        return true
+      end
+    when "White"
+      if end_space[0] == current_space[0] - 1 && end_space[1] == current_space [1] + 1 && board.board[end_space[0]][end_space[1]].color != "White"
+        return true
+      elsif end_space[0] == current_space[0] - 1 && end_space[1] == current_space [1] - 1 && board.board[end_space[0]][end_space[1]].color != "White"
+        return true  
+      end
+    end
+    false
+  end
 end
 
 class Rooke < Piece
@@ -122,6 +150,7 @@ class King < Piece
 end
 
 class Game
+  attr_reader :current_color
 
   def initialize
     @player_1 = ""
@@ -130,9 +159,11 @@ class Game
     @current_color = ""
     @piece_to_move = ""
     @space_to_move_to = ""
+    @game_piece = ""
     @board = Board.new
   end
 
+  #Gather's and sets players' names
   def welcome_message
     puts "\n\nWelcome to chess!\n\nPlayer 1, what would you like to be called?\n"
 
@@ -148,10 +179,8 @@ class Game
     puts "\nThank you #{@player_2.name}\n\n"
   end
 
+  #Message that appears before each turn or after an invalid user input
   def turn_message
-    #@board.board[2][2] = Pawn.new("Black", "265F")
-    #@board.board[1][2] = nil
-    #@board.display
     @board.display
     puts "Your turn, #{@current_player}. Please select the piece you would like to move\nby typing in its coordinates, separated by a comma and a space. ie. 6, 2\n\n"
 
@@ -160,20 +189,35 @@ class Game
 
   def find_piece_type
     if format_origin_space_input && format_destination_space_input
-      @piece_class = @board.board[@piece_to_move[0]][@piece_to_move[1]].class
-      
-      @board.board[@space_to_move_to[0]][@space_to_move_to[1]] = @board.board[@piece_to_move[0]][@piece_to_move[1]]
-      @board.board[@piece_to_move[0]][@piece_to_move[1]] = nil
-      @board.display
+
+      #sets @piece_to_move as actual instance of a class, rather than an array
+      @game_piece = @board.board[@piece_to_move[0]][@piece_to_move[1]]
+
+      if @game_piece.move?(@piece_to_move, @space_to_move_to, @current_color) || @game_piece.attackable_piece?(@piece_to_move, @space_to_move_to, @current_color, @board)
+        #sets game_piece in the desired space on the board
+        @board.board[@space_to_move_to[0]][@space_to_move_to[1]] = @game_piece
+
+        #removes the game piece from it's previous spot on the board
+        @board.board[@piece_to_move[0]][@piece_to_move[1]] = nil
+        switch_players
+        #display the board with updated piece positions
+        turn_message
+      else
+        error_message("piece limitation")
+        turn_message
+      end
     else
       turn_message 
     end
   end
 
+  #Turns user's input into an array
   def format_origin_space_input
     acceptable_input = ["0", "1", "2", "3", "4", "5", "6", "7"]
     @piece_to_move = gets.chomp.split(", ")
 
+    #Makes sure user input was only 2 numbers in the form of strings that also are both included in accectable_input array
+    #Turns those strings into integers
     if @piece_to_move.length == 2 && @piece_to_move.all? { |num| acceptable_input.include?(num) }
       @piece_to_move = @piece_to_move.map! { |string| string.to_i }
       return validate_piece_to_move_input(@piece_to_move[0], @piece_to_move[1])
@@ -183,12 +227,15 @@ class Game
     end
   end
 
+  #Turns user's input into an array
   def format_destination_space_input
     acceptable_input = ["0", "1", "2", "3", "4", "5", "6", "7"]
 
     puts "Please select the space you would like to move to\nby typing in its coordinates, separated by a comma and a space. ie. 6, 2\n\n"
     @space_to_move_to = gets.chomp.split(", ")
 
+    #Makes sure user input was only 2 numbers in the form of strings that also are both included in accectable_input array
+    #Turns those strings into integers
     if @space_to_move_to.length == 2 && @space_to_move_to.all? { |num| acceptable_input.include?(num) }
       @space_to_move_to = @space_to_move_to.map! { |string| string.to_i }
       return validate_space_to_move_to(@space_to_move_to[0], @space_to_move_to[1])
@@ -196,8 +243,6 @@ class Game
       error_message("bad range")
       return false
     end
-    #@board.board[@piece_to_move[0]][@piece_to_move[1]] = nil
-    #@board.display
   end
 
   def validate_piece_to_move_input(input_1, input_2)
@@ -214,30 +259,36 @@ class Game
   end
 
   def validate_space_to_move_to(input_1, input_2)
-    if @board.board[input_1][input_2].nil?
+    if @board.board[input_1][input_2].nil? || @game_piece.attackable_piece?(@piece_to_move, @space_to_move_to, @current_color, @board)
       return true
-    #else
-     # error_message("no piece")
-      #return false
+    else
+     error_message("occupied")
+      return false
     end
   end
 
   def error_message(error_type)
     case error_type
     when "bad range"
-      #@board.display
       puts "\n\nPlease make sure consists of two numbers betweem 0 and 7"
     when "no piece"
-      #@board.display
       puts "\n\nYou must enter a space that currently has one of your pieces"
+    when "piece limitation"
+      puts "\n\nA #{@game_piece.class} can not move to #{@space_to_move_to}"
+    
+    when "occupied"
+      puts "\n\nThere is aleady a piece at #{@space_to_move_to}, and you can not capture it"
     end
   end
 
+  #Switches who is the crrent player to determine who's turn is next
   def switch_players
     if @current_player == @player_1.name
       @current_player = @player_2.name
+      @current_color = @player_2.color
     elsif @current_player == @player_2.name
       @current_player = @player_1.name
+      @current_color = @player_1.color
     end
   end
 end
